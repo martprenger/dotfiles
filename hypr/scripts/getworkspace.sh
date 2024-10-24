96e2de2b4205913286440b75bdac003bff0d7fb2
martprenger <mart.prenger@gmail.com>
2024-10-24 21:44:03 +0200

    added screenshot

--- hypr/scripts/getworkspace.sh
+++ hypr/scripts/getworkspace.sh
@@ -14,10 +14,7 @@ active_monitor_id=$(echo "$hyprctl_output" | jq '.[] | select(.focused == true) 
 
 # Assign values based on the screen name
 case "$active_monitor_id" in
-    1) value=0 ;;
-    0) value=1 ;;
-    2) value=2 ;;
-    *) value=999 ;; # Default value for unknown screen names
+    *) value=0 ;; # Default value for unknown screen names
 esac
 
 # Perform the calculation (multiply by 10 and add to the parameter)

a67b0fd3838ca86b52cbdda7577b953c25902d0e
mart prenger <mart.prenger@gmail.com>
2024-11-17 21:19:47 +0100

    made events scrolable

--- ags/modules/center/DateColumn.ts
+++ ags/modules/center/DateColumn.ts
@@ -2,78 +2,83 @@ import GLib from "gi://GLib";
 import DateTime from "../../services/datetime.js";
 import calendarServer from "../../services/ecal.js";
 
-const events = Widget.Box({
-    vertical: true,
-    homogeneous: false,
-    className: "events",
-    vpack: "fill",
-    hpack: "fill",
-    children: calendarServer.bind("events").as((e) =>
-        e
-            .sort((a, b) => a.start - b.start)
-            .map((event) => {
-                const time = Widget.Label({
-                    className: "time",
-                    hpack: "start",
-                    justification: "left",
-                    wrap: true
-                });
+const events = Widget.Scrollable({
+    vexpand: true,
+    hscroll: "never",
+    class_name: "notification-scrollable",
+    child: Widget.Box({
+        vertical: true,
+        homogeneous: false,
+        className: "events",
+        vpack: "fill",
+        hpack: "fill",
+        children: calendarServer.bind("events").as((e) =>
+            e
+                .sort((a, b) => a.start - b.start)
+                .map((event) => {
+                    const time = Widget.Label({
+                        className: "time",
+                        hpack: "start",
+                        justification: "left",
+                        wrap: true
+                    });
 
-                const start = GLib.DateTime.new_from_unix_utc(event.start);
+                    const start = GLib.DateTime.new_from_unix_utc(event.start);
 
-                if (event.whole_day) {
-                    if (event.single_day) time.label = "O dia inteiro";
-                    else {
-                        const end = GLib.DateTime.new_from_unix_utc(event.end - 1);
-                        time.label = `${start.format("%a %d")} - ${end.format("%a %d")}`;
+                    if (event.whole_day) {
+                        if (event.single_day) time.label = "O dia inteiro";
+                        else {
+                            const end = GLib.DateTime.new_from_unix_utc(event.end - 1);
+                            time.label = `${start.format("%a %d")} - ${end.format("%a %d")}`;
+                        }
+                    } else {
+                        const end = GLib.DateTime.new_from_unix_utc(event.end);
+                        if (event.single_day) time.label = `${start.format("%R")} - ${end.format("%R")}`;
+                        else time.label = `${start.format("%a %d, %R")} - ${end.format("%a %d, %R")}`;
                     }
-                } else {
-                    const end = GLib.DateTime.new_from_unix_utc(event.end);
-                    if (event.single_day) time.label = `${start.format("%R")} - ${end.format("%R")}`;
-                    else time.label = `${start.format("%a %d, %R")} - ${end.format("%a %d, %R")}`;
-                }
 
-                return Widget.Box({
-                    vertical: true,
-                    homogeneous: false,
-                    hpack: "fill",
-                    vpack: "fill",
-                    className: "event",
-                    css: event.color ? `color: ${event.color};` : null,
-                    children: [
-                        Widget.Label({
-                            hpack: "start",
-                            justification: "left",
-                            wrap: true,
-                            className: "summary",
-                            visible: event.summary ? event.summary.length > 0 : false,
-                            label: event.summary ? event.summary : ""
-                        }),
-                        time,
-
-                        ...event.description.map((d) =>
+                    return Widget.Box({
+                        vertical: true,
+                        homogeneous: false,
+                        hpack: "fill",
+                        vpack: "fill",
+                        className: "event",
+                        css: event.color ? `color: ${event.color};` : null,
+                        children: [
                             Widget.Label({
                                 hpack: "start",
                                 justification: "left",
                                 wrap: true,
-                                label: d
-                            })
-                        ),
+                                className: "summary",
+                                visible: event.summary ? event.summary.length > 0 : false,
+                                label: event.summary ? event.summary : ""
+                            }),
+                            time,
 
-                        event.location?.length > 0
-                            ? Widget.Label({
-                                  className: "location",
-                                  label: event.location,
-                                  hpack: "start",
-                                  justification: "left",
-                                  wrap: true
-                              })
-                            : null
-                    ]
-                });
-            })
-    ),
-    visible: calendarServer.bind("events").as((e) => e.length > 0)
+                            ...event.description.map((d) =>
+                                Widget.Label({
+                                    hpack: "start",
+                                    justification: "left",
+                                    wrap: true,
+                                    label: d
+                                })
+                            ),
+
+                            event.location?.length > 0
+                                ? Widget.Label({
+                                      className: "location",
+                                      label: event.location,
+                                      hpack: "start",
+                                      justification: "left",
+                                      wrap: true
+                                  })
+                                : null
+                        ]
+                    });
+                })
+        ),
+        visible: calendarServer.bind("events").as((e) => e.length > 0)
+    })
 });
 
 export default () =>
